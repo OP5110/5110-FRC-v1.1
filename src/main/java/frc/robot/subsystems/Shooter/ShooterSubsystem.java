d33e81415a1c9f40c67616e687eefb334b103e05
OP5110 <162637728+OP5110@users.noreply.github.com>
2024-03-19 16:07:42 -0400

    files added

---
+++ src/main/java/frc/robot/subsystems/Shooter/ShooterSubsystem.java
@@ -0,0 +1,47 @@
+package frc.robot.subsystems.Shooter;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+//import com.revrobotics.*;
+import com.revrobotics.CANSparkLowLevel.MotorType;
+import com.revrobotics.CANSparkBase.IdleMode;
+import com.revrobotics.CANSparkMax;
+
+public class ShooterSubsystem extends SubsystemBase {
+    private final CANSparkMax shooterMotor;
+    private final CANSparkMax shooterMotor2;
+    public ShooterSubsystem() {
+        shooterMotor = new CANSparkMax(16, MotorType.kBrushless);
+        shooterMotor2 = new CANSparkMax(17, MotorType.kBrushless);
+        shooterMotor.restoreFactoryDefaults();
+        shooterMotor2.restoreFactoryDefaults();
+//        shooterMotor.setIdleMode(IdleMode.kBrake);
+    }
+
+    public Command runShooter(double speed){
+        return this.runOnce(() -> {
+            shooterMotor.set(speed);
+            shooterMotor2.set(speed);
+        });
+    }
+
+// Auto
+
+    public Command stopShooter(){
+        return this.runOnce(() -> {
+            shooterMotor.set(0);
+            shooterMotor2.set(0);
+        });
+    }
+
+     public Command autoShooterRun(){
+        return new InstantCommand(
+            () -> {
+                shooterMotor.set(-1);
+                shooterMotor2.set(-1);
+            });
+    }
+}
+
